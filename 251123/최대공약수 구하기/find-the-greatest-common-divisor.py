0f8bf49a030e8bfb703b21dfad1dc1eb62034e11
namgijeong <dreaming1817@gmail.com>
2025-11-23 16:35:50 +0900

    Title: 최대공약수 구하기, Time: 76ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 251123/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 251123/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,14 +1,23 @@
 n, m = map(int, input().split())
 
 # Please write your code here.
+# def max_yaksu(n,m):
+#     if n>m:
+#         result = n%m
+#     else:
+#         result = m%n    
+
+#     if result == 0:
+#         result = m
+#     print(result)
+
 def max_yaksu(n,m):
-    if n>m:
-        result = n%m
-    else:
-        result = m%n    
+    gcd = 1
+    try = min(n,m) + 1
+    for i in range(1, try):
+        if n%i == 0 and m%i == 0:
+            gcd = i
 
-    if result == 0:
-        result = m
-    print(result)
-
+    print(gcd)
+    
 max_yaksu(n,m)    

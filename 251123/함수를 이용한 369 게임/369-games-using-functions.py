8e6eaf91257a7df756bd86f4f5cbb9d289f05abe
namgijeong <dreaming1817@gmail.com>
2025-11-23 23:45:05 +0900

    Title: 함수를 이용한 369 게임, Time: 60ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 251123/함수를 이용한 369 게임/369-games-using-functions.py
+++ 251123/함수를 이용한 369 게임/369-games-using-functions.py
@@ -3,7 +3,7 @@ a, b = map(int, input().split())
 def include369(n):
     digit_10 = n//10
     digit_1 = n%10
-    if digit_10%3 == 0 || digit_1%3 ==0:
+    if digit_10%3 == 0 or digit_1%3 ==0:
         return True
     else:
         return False    
@@ -11,7 +11,7 @@ def include369(n):
 def count369(a,b):
     count = 0
     for i in range(a,b+1):
-        if i%3 == 0 || include369(i):
+        if i%3 == 0 or include369(i):
             count += 1
 
     return count

427a35f3d806e668db7b41919e5b72ce23c2fd3b
namgijeong <dreaming1817@gmail.com>
2025-11-24 21:26:51 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 251124/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
+++ 251124/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
@@ -12,9 +12,9 @@ def isDigitSumEven(n):
     mok =  n
     
     sum_n = 0
-    while mok != 0:
+    while mok > 0:
         namage = mok%10
-        mok = mok / 10      
+        mok = mok // 10      
         sum_n += namage  
 
     if sum_n % 2 == 0:
@@ -26,6 +26,7 @@ def isDigitSumEven(n):
 count = 0
 for i in range(a, b+1):
     if isSosu(i) and isDigitSumEven(i):
+    
         count += 1
 
 

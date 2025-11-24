757a061f241e85fdffe9613a061cf302a77de8b3
namgijeong <dreaming1817@gmail.com>
2025-11-24 21:54:11 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 73ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 251124/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 251124/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -5,22 +5,36 @@ b = list(map(int, input().split()))
 b_start_index = 0
 a_start_index = 0
 
-for i in range(n1):
-    for j in range(n2):
-        if a[i] == b[j]:
-            b_start_index = j
-            a_start_index = i
-            break
+def find_start_index(n1,n2,a,b):
+    #바깥변수를 인식하려면 어쩔수 없이 global
+    global b_start_index
+    global a_start_index
+
+    for i in range(n1):
+        for j in range(n2):
+            if a[i] == b[j]:
+                # print(a[i], ' ',b[j])
+                # print(i, ' ',j)
+                b_start_index = j
+                a_start_index = i
+                # print(a_start_index)
+                # print(b_start_index)
+                return
 
 
-is_true = true
+find_start_index(n1,n2,a,b)
+
+
+is_true = True
 for i in range(n2):
     if a[a_start_index] != b[b_start_index]:
         is_true = False
         break
-else:
-    is_true = True    
+    else:
+        a_start_index += 1    
+        b_start_index += 1
 
+   
 
 if is_true == True:
     print('Yes')

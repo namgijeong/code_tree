fb5da0afead2df9b21b8f3e72bdd363b3ab3aca9
namgijeong <dreaming1817@gmail.com>
2025-11-24 12:07:24 +0900

    Title: 함수를 이용한 소수 판별, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251124/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 251124/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -4,9 +4,12 @@ def isSosu(a,b):
     sum_n = 0
 
     for i in range(a, b+1):
+        sosu_flag = True
         for j in range(2,i):
             if i%j == 0:
-                sum_n += i
+                sosu_flag = False
+        if sosu_flag == True:
+            sum_n += i        
 
     return sum_n
 

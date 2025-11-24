7755861e1353dd8d2ac41704137eebddb18eccd1
namgijeong <dreaming1817@gmail.com>
2025-11-24 21:17:35 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 80ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251124/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
@@ -0,0 +1,32 @@
+a, b = map(int, input().split())
+
+def isSosu(n):
+    for i in range(2,n):
+        if n%i == 0:
+            return False
+
+    return True
+
+
+def isDigitSumEven(n):
+    mok =  n
+    
+    sum_n = 0
+    while mok != 0:
+        namage = n%10
+        mok = mok / 10      
+        sum_n += namage  
+
+    if sum_n % 2 == 0:
+        return True
+    else:
+        return False
+
+
+count = 0
+for i in range(a, b+1):
+    if isSosu(i) and isDigitSumEven(n):
+        count += 1
+
+
+print(count)

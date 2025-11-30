b71f6d83feeee4d0a187075d5795485a197cbde1
namgijeong <dreaming1817@gmail.com>
2025-11-30 16:51:27 +0900

    Title: palindrome 여부 판단하기, Time: 75ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 251130/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
+++ 251130/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
@@ -2,15 +2,19 @@ A = input()
 
 def is_palindrome(A):
     if len(A) == 1:
-        return True
+        print('Yes')
+        return 
 
     else:
         if A[0] == A[-1]:
             return is_palindrome(A[1:-1])
         else:
-            return False    
+            print("No")
+            return     
 
-if is_palindrome(A):
-    print("Yes")
-else:
-    print("No")            
+# if is_palindrome(A):
+#     print("Yes")
+# else:
+#     print("No")    
+
+if is_palindrome(A)

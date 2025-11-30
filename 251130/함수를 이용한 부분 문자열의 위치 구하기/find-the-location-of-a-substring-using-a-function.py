f1873fdc2099d5ea70b6518f8595878f87f32aa9
namgijeong <dreaming1817@gmail.com>
2025-11-30 18:15:16 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 81ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 251130/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 251130/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -1,12 +1,11 @@
 text = input()
 pattern = input()
 
-def is_same(text_n, pattern_n, text_start_index,pattern_start_index):
+def is_same(text_n, pattern_n, text_start_index):
     count = 0
-    for i in range(text_start_index, text_n):
-        for j in range(pattern_start_index, pattern_n):
-            if text[i] == pattern[j]:
-                count += 1
+    for i in range(pattern_n):
+        if text[text_start_index + i] == pattern[i]:
+            count += 1
 
     if count == pattern_n:
         return True
@@ -18,12 +17,11 @@ def is_part():
     text_n = len(text)
     pattern_n = len(pattern)
     
-    for i in range(text_n):
-        for j in range(pattern_n):
-            if text[i] == pattern[j]:
-                if is_same(text_n, pattern_n,i,j):
-                    print(i)
-                    return
+    for i in range(text_n): 
+        if text[i] == pattern[0]:
+            if is_same(text_n, pattern_n,i):
+                print(i)
+                return
 
     print(-1)
     return         

d0d2b0998be8b9898e9c7932e6d8ed50bb5b34a6
namgijeong <dreaming1817@gmail.com>
2025-11-30 17:08:16 +0900

    Title: 두 정수에 대한 연산값, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251130/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -0,0 +1,19 @@
+a, b = map(int, input().split())
+
+def number_print(a, b):
+    if a > b:
+        a = a+25
+        b = b*2
+
+    else:
+        b = b+25
+        a = a*2
+
+    return a,b
+
+
+a, b = number_print(a,b)
+
+
+print(a,b)
+        
